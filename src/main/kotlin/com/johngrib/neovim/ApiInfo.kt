dbd12208b1f1577148bf0fccef282cbc31faaf25
JohnGrib <johngrib82@gmail.com>
2023-10-03 21:54:07 +0900

    Add neovim/ApiInfo

---
+++ src/main/kotlin/com/johngrib/neovim/ApiInfo.kt
@@ -0,0 +1,8 @@
+package com.johngrib.neovim
+
+class ApiInfo(val list: List<*>) {
+    private val map: Map<*, *> by lazy {
+        list.last() as Map<*, *>
+    }
+    val channelId: Int by lazy { list.first() as Int }
+}
